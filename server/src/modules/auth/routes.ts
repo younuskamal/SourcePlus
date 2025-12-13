b3abde9515ef41fb1a30fa8fb610acc8b6ab3be9
YOUNUS KAMAL <136609049+younuskamal@users.noreply.github.com>
2025-12-13 05:58:38 +0300

    log2

--- server/src/modules/auth/routes.ts
+++ server/src/modules/auth/routes.ts
@@ -28,7 +28,7 @@ export default async function authRoutes(app: FastifyInstance) {
     if ((user as any).status && (user as any).status !== 'APPROVED') {
       return reply
         .code(403)
-        .send({ message: 'الحساب بانتظار الموافقة أو معلق' });
+        .send({ message: 'الحساب بانتظار تفعيل الإدارة' });
     }
 
     const accessToken = app.jwt.sign(
